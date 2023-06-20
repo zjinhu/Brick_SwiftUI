3942400f7d7449943e67ccf9f1583927132e44c3
jinhu <814030966@qq.com>
2023-06-20 11:01:14 +0800

    package

---
+++ Package@swift-5.0.swift.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 5.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(name: "Brick_SwiftUI")
+
+package.platforms = [.iOS(.v14),
+                     .tvOS(.v14),
+                     .watchOS(.v7),
+                     .macOS(.v11)]
+
+package.products = [
+    .library(
+        name: "Brick_SwiftUI",
+        targets: ["Brick_SwiftUI"]),
+]
+
+package.targets = [
+    .target(
+        name: "Brick_SwiftUI"),
+]
+
+package.swiftLanguageVersions = [.v5]
