50316ac9efcfffd4acc03560b6c70a4b5b1159d1
jinhu <814030966@qq.com>
2023-06-26 11:10:19 +0800

    Update RightArrow.swift

--- Sources/Brick_SwiftUI/SwiftUI/RightArrow.swift
+++ Sources/Brick_SwiftUI/SwiftUI/RightArrow.swift
@@ -8,19 +8,22 @@
 import SwiftUI
 
 extension View {
-    public func rightArrow() -> some View {
+    public func rightArrow(color: Color = Color(.systemGray2),
+                           font: Font = Font.footnote.weight(.semibold)) -> some View {
         HStack {
             self
             Spacer()
-            Chevron()
+            Chevron(color: color, font: font)
         }
     }
 }
 
 struct Chevron: View {
+    let color: Color
+    let font: Font
     var body: some View {
         Image(systemName: .chevronRight)
-            .foregroundColor(.secondary)
-            .font(.callout.weight(.semibold))
+            .foregroundColor(color)
+            .font(font)
     }
 }
