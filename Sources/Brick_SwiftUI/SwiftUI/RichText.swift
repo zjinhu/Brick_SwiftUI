a6f2c00115b813208f3d42ac59687117a351343a
jinhu <814030966@qq.com>
2023-06-28 10:40:35 +0800

    Update RichText.swift

--- Sources/Brick_SwiftUI/SwiftUI/RichText.swift
+++ Sources/Brick_SwiftUI/SwiftUI/RichText.swift
@@ -7,122 +7,124 @@
 
 import SwiftUI
 
+public typealias RichText = ArrayBuilder<Text>
+
 extension Text {
     
     @_disfavoredOverload
-    @inlinable
-    public init<S: StringProtocol>(
-        separator: S,
-        @RichText blocks: () -> [Text]
-    ) {
-        self.init(separator: Text(separator), blocks: blocks)
-    }
-    
-    @inlinable
-    public init(
-        separator: LocalizedStringKey,
-        @RichText blocks: () -> [Text]
-    ) {
-        self.init(separator: Text(separator), blocks: blocks)
-    }
-    
-    @inlinable
-    public init(
-        separator: Text,
-        @RichText blocks: () -> [Text]
-    ) {
-        let blocks = blocks()
-        switch blocks.count {
-        case 0:
-            self = Text(verbatim: "")
-            
-        case 1:
-            self = blocks[0]
-            
-        default:
-            self = blocks.dropFirst().reduce(into: blocks[0]) { result, text in
-                result = result + separator + text
+        @inlinable
+        public init<S: StringProtocol>(
+            separator: S,
+            @RichText blocks: () -> [Text]
+        ) {
+            self.init(separator: Text(separator), blocks: blocks)
+        }
+
+        @inlinable
+        public init(
+            separator: LocalizedStringKey,
+            @RichText blocks: () -> [Text]
+        ) {
+            self.init(separator: Text(separator), blocks: blocks)
+        }
+
+        @inlinable
+        public init(
+            separator: Text,
+            @RichText blocks: () -> [Text]
+        ) {
+            let blocks = blocks()
+            switch blocks.count {
+            case 0:
+                self = Text(verbatim: "")
+
+            case 1:
+                self = blocks[0]
+
+            default:
+                self = blocks.dropFirst().reduce(into: blocks[0]) { result, text in
+                    result = result + separator + text
+                }
             }
         }
-    }
 }
 
 @frozen
 @resultBuilder
-public struct RichText{
-    
+public struct ArrayBuilder<Element> {
+
     @inlinable
-    public static func buildBlock() -> [Optional<Text>] {
+    public static func buildBlock() -> [Optional<Element>] {
         []
     }
-    
+
     @inlinable
     public static func buildPartialBlock(
-        first: [Optional<Text>]
-    ) -> [Optional<Text>] {
+        first: [Optional<Element>]
+    ) -> [Optional<Element>] {
         first
     }
-    
+
     @inlinable
     public static func buildPartialBlock(
-        accumulated: [Optional<Text>],
-        next: [Optional<Text>]
-    ) -> [Optional<Text>] {
+        accumulated: [Optional<Element>],
+        next: [Optional<Element>]
+    ) -> [Optional<Element>] {
         accumulated + next
     }
-    
+
     @inlinable
     public static func buildExpression(
-        _ expression: Text
-    ) -> [Optional<Text>] {
+        _ expression: Element
+    ) -> [Optional<Element>] {
         [expression]
     }
-    
+
     @inlinable
     public static func buildEither(
-        first component: [Optional<Text>]
-    ) -> [Optional<Text>] {
+        first component: [Optional<Element>]
+    ) -> [Optional<Element>] {
         component
     }
-    
+
     @inlinable
     public static func buildEither(
-        second component: [Optional<Text>]
-    ) -> [Optional<Text>] {
+        second component: [Optional<Element>]
+    ) -> [Optional<Element>] {
         component
     }
-    
+
     @inlinable
     public static func buildOptional(
-        _ component: [Optional<Text>]?
-    ) -> [Optional<Text>] {
+        _ component: [Optional<Element>]?
+    ) -> [Optional<Element>] {
         component ?? []
     }
-    
+
     @inlinable
     public static func buildLimitedAvailability(
-        _ component: [Optional<Text>]
-    ) -> [Optional<Text>] {
+        _ component: [Optional<Element>]
+    ) -> [Optional<Element>] {
         component
     }
-    
+
     @inlinable
     public static func buildArray(
-        _ components: [Optional<Text>]
-    ) -> [Optional<Text>] {
+        _ components: [Optional<Element>]
+    ) -> [Optional<Element>] {
         components
     }
-    
+
     @inlinable
     public static func buildBlock(
-        _ components: [Optional<Text>]...
-    ) -> [Optional<Text>] {
+        _ components: [Optional<Element>]...
+    ) -> [Optional<Element>] {
         components.flatMap { $0 }
     }
-    
+
     public static func buildFinalResult(
-        _ component: [Optional<Text>]
-    ) -> [Text] {
+        _ component: [Optional<Element>]
+    ) -> [Element] {
         component.compactMap { $0 }
     }
 }
