4c0cbccad689a799ee34c48aec1d2467173a0d94
jinhu <814030966@qq.com>
2023-07-07 12:00:11 +0800

    Update Screen++.swift

--- Sources/Brick_SwiftUI/Utilities/Screen++.swift
+++ Sources/Brick_SwiftUI/Utilities/Screen++.swift
@@ -8,6 +8,9 @@
 import UIKit
 import Foundation
 
+
+
+#if os(iOS) || os(tvOS)
 extension UIWindow {
     /// get window
     public static var keyWindow: UIWindow? {
@@ -31,23 +34,6 @@ extension UIWindowScene {
     }
 }
  
-
-extension UIScreen {
-    public static var safeArea: UIEdgeInsets {
-        UIApplication.shared
-            .connectedScenes
-            .compactMap { $0 as? UIWindowScene }
-            .flatMap(\.windows)
-            .first(where: \.isKeyWindow)?
-            .safeAreaInsets ?? .zero
-    }
-    
-    /// 当前屏幕状态 宽度
-    public static var realHeight = max(UIScreen.main.bounds.width, UIScreen.main.bounds.height)
-    /// 当前屏幕状态 高度
-    public static var realWidth = min(UIScreen.main.bounds.width, UIScreen.main.bounds.height)
-}
-
 private extension UIScene.ActivationState {
     var sortPriority: Int {
         switch self {
@@ -59,3 +45,35 @@ private extension UIScene.ActivationState {
         }
     }
 }
+
+public class Screen {
+    static var safeArea: UIEdgeInsets = UIScreen.safeArea
+    /// 当前屏幕状态 宽度
+    static var realHeight = max(UIScreen.main.bounds.width, UIScreen.main.bounds.height)
+    /// 当前屏幕状态 高度
+    static var realWidth = min(UIScreen.main.bounds.width, UIScreen.main.bounds.height)
+}
+
+fileprivate extension UIScreen {
+    static var safeArea: UIEdgeInsets {
+        UIApplication.shared
+            .connectedScenes
+            .compactMap { $0 as? UIWindowScene }
+            .flatMap(\.windows)
+            .first(where: \.isKeyWindow)?
+            .safeAreaInsets ?? .zero
+    }
+}
+ 
+#elseif os(macOS)
+public class Screen {
+    static var safeArea: NSEdgeInsets = NSScreen.safeArea
+}
+fileprivate extension NSScreen {
+    static var safeArea: NSEdgeInsets =
+    NSApplication.shared
+        .mainWindow?
+        .contentView?
+        .safeAreaInsets ?? .init(top: 0, left: 0, bottom: 0, right: 0)
+}
+#endif
