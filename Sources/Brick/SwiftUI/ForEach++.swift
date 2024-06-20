05d3685a6e8b12d6bf48d7724612220b40be1cde
jinhu <814030966@qq.com>
2024-06-20 13:54:01 +0800

    ForEach

--- Sources/Brick/SwiftUI/ForEach++.swift
+++ Sources/Brick/SwiftUI/ForEach++.swift
@@ -8,6 +8,45 @@
 import SwiftUI
 
 extension ForEach {
+    @_disfavoredOverload
+    @inlinable
+    public init<_Data: RandomAccessCollection>(
+        _ data: _Data,
+        @ViewBuilder content: @escaping (_Data.Index, _Data.Element) -> Content
+    ) where Data == Array<(_Data.Index, _Data.Element)>, ID == _Data.Index, Content: View {
+        let elements = Array(zip(data.indices, data))
+        self.init(elements, id: \.0) { index, element in
+            content(index, element)
+        }
+    }
+
+    @inlinable
+    public init<_Data: RandomAccessCollection>(
+        _ data: _Data,
+        id: KeyPath<_Data.Element, ID>,
+        @ViewBuilder content: @escaping (_Data.Index, _Data.Element) -> Content
+    ) where Data == Array<(_Data.Index, _Data.Element)>, Content: View {
+        let elements = Array(zip(data.indices, data))
+        let elementPath: KeyPath<(_Data.Index, _Data.Element), _Data.Element> = \.1
+        self.init(elements, id: elementPath.appending(path: id)) { index, element in
+            content(index, element)
+        }
+    }
+
+    @inlinable
+    public init<_Data: RandomAccessCollection>(
+        _ data: _Data,
+        @ViewBuilder content: @escaping (_Data.Index, _Data.Element) -> Content
+    ) where Data == Array<(_Data.Index, _Data.Element)>, _Data.Element: Identifiable, ID == _Data.Element.ID, Content: View {
+        let elements = Array(zip(data.indices, data))
+        self.init(elements, id: \.1.id) { index, element in
+            content(index, element)
+        }
+    }
+}
+
+
+extension ForEach {
     public var isEmpty: Bool {
         data.isEmpty
     }
