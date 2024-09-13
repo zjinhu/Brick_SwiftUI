4bbf9c0784b1319a1d3781a725870d4b52b25f2e
jinhu <814030966@qq.com>
2024-09-13 16:35:11 +0800

    Create Task+.swift

---
+++ Sources/Brick/SwiftUI/Task+.swift
@@ -0,0 +1,16 @@
+//
+//  SwiftUIView.swift
+//  Brick_SwiftUI
+//
+//  Created by iOS on 9/13/24.
+//
+
+import SwiftUI
+
+extension Task where Success == Never, Failure == Never {
+    // 扩展 Task，支持以秒为单位进行暂停
+    public static func sleep(seconds: TimeInterval) async throws {
+        let nanoseconds = UInt64(seconds * 1_000_000_000)
+        try await Task.sleep(nanoseconds: nanoseconds)
+    }
+}
