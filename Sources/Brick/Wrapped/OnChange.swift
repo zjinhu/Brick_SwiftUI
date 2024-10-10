0ac0261c3277171066ed3f1dbfe9b3239a982ef1
jinhu <814030966@qq.com>
2024-10-10 15:47:05 +0800

    onChange

--- Sources/Brick/Wrapped/OnChange.swift
+++ Sources/Brick/Wrapped/OnChange.swift
@@ -43,35 +43,35 @@ public extension Brick where Wrapped: View {
     }
   
     //兼容ios13
-    @ViewBuilder
-    func onChange<Value: Equatable>(of value: Value, perform action: @escaping (Value) -> Void) -> some View {
-        if #available(iOS 14, tvOS 14, macOS 11, watchOS 7, *) {
-            wrapped.onChange(of: value, perform: action)
-        } else {
-            wrapped.modifier(ChangeModifier(value: value, action: action))
-        }
-    }
+//    @ViewBuilder
+//    func onChange<Value: Equatable>(of value: Value, perform action: @escaping (Value) -> Void) -> some View {
+//        if #available(iOS 14, tvOS 14, macOS 11, watchOS 7, *) {
+//            wrapped.onChange(of: value, perform: action)
+//        } else {
+//            wrapped.modifier(ChangeModifier(value: value, action: action))
+//        }
+//    }
 
 }
 ///ios13
-private struct ChangeModifier<Value: Equatable>: ViewModifier {
-    let value: Value
-    let action: (Value) -> Void
-
-    @State var oldValue: Value?
-
-    init(value: Value, action: @escaping (Value) -> Void) {
-        self.value = value
-        self.action = action
-        _oldValue = .init(initialValue: value)
-    }
-
-    func body(content: Content) -> some View {
-        content
-            .onReceive(Just(value)) { newValue in
-                guard newValue != oldValue else { return }
-                action(newValue)
-                oldValue = newValue
-            }
-    }
-}
+//private struct ChangeModifier<Value: Equatable>: ViewModifier {
+//    let value: Value
+//    let action: (Value) -> Void
+//
+//    @State var oldValue: Value?
+//
+//    init(value: Value, action: @escaping (Value) -> Void) {
+//        self.value = value
+//        self.action = action
+//        _oldValue = .init(initialValue: value)
+//    }
+//
+//    func body(content: Content) -> some View {
+//        content
+//            .onReceive(Just(value)) { newValue in
+//                guard newValue != oldValue else { return }
+//                action(newValue)
+//                oldValue = newValue
+//            }
+//    }
+//}
