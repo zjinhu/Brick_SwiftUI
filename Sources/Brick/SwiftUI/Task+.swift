26d67d7639ec4771feea82fd12a9c395aa3b3be6
虎 <814030966@qq.com>
2025-01-09 15:17:13 +0800

    Update Task+.swift

--- Sources/Brick/SwiftUI/Task+.swift
+++ Sources/Brick/SwiftUI/Task+.swift
@@ -11,6 +11,6 @@ extension Task where Success == Never, Failure == Never {
     // 扩展 Task，支持以秒为单位进行暂停
     public static func sleep(seconds: TimeInterval) async throws {
         let nanoseconds = UInt64(seconds * 1_000_000_000)
-        try await Task.sleep(nanoseconds: nanoseconds)
+        try? await Task.sleep(nanoseconds: nanoseconds)
     }
 }
