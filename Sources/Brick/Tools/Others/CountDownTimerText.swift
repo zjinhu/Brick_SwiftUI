a4a706b41d95ab6e679930578436713a4ae18571
虎 <814030966@qq.com>
2025-08-12 11:12:46 +0800

    倒计时添加手动模式

--- Sources/Brick/Tools/Others/CountDownTimerText.swift
+++ Sources/Brick/Tools/Others/CountDownTimerText.swift
@@ -8,46 +8,65 @@
 import SwiftUI
 
 public struct CountDownTimerText: View {
+    public enum Mode {
+        case auto
+        case manual
+    }
+    
     @State private var timeRemaining = 0
-
-    @State private var timeCounting = true
+    @State private var timeCounting = false
     @State private var timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
-
+    
     let timeUnit: String
     let timeCount: Int
-    let text: String
+    let titleWhenFinished: String
+    let titleWhenIdle: String
+    let mode: Mode
     public typealias ResumeAction = () -> Void
     let resumeAction: ResumeAction
-    public init(text: String,
-                timeCount: Int = 59,
+    
+    public init(timeCount: Int = 59,
                 timeUnit: String = "",
+                titleWhenIdle: String,
+                titleWhenFinished: String,
+                mode: Mode = .auto,
                 resumeAction: @escaping ResumeAction) {
-        self.text = text
         self.timeCount = timeCount
         self.timeUnit = timeUnit
+        self.titleWhenIdle = titleWhenIdle
+        self.titleWhenFinished = titleWhenFinished
+        self.mode = mode
         self.resumeAction = resumeAction
         _timeRemaining = State(wrappedValue: timeCount)
+        if mode == .auto {
+            _timeCounting = State(wrappedValue: true)
+        } else {
+            _timeCounting = State(wrappedValue: false)
+        }
     }
     
     public var body: some View {
-        if !timeCounting{
-            Text(text)
+        if !timeCounting {
+            Text(mode == .manual ? titleWhenIdle : titleWhenFinished)
                 .underline()
                 .onTapGesture {
-                    timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
-                    timeCounting.toggle()
-                    resumeAction()
+                    if mode == .manual {
+                        timeRemaining = timeCount
+                        timeCounting = true
+                        timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+                        resumeAction()
+                    }
                 }
-        }else{
+        } else {
             Text("\(timeRemaining)\(timeUnit)")
                 .underline()
-                .onReceive(timer) { time in
+                .onReceive(timer) { _ in
                     if timeRemaining > 1 {
                         timeRemaining -= 1
-                    }else{
+                    } else {
                         timer.upstream.connect().cancel()
-                        timeRemaining = 59
-                        timeCounting.toggle()
+                        timeRemaining = timeCount
+                        timeCounting = false
                     }
                 }
         }
@@ -55,7 +74,12 @@ public struct CountDownTimerText: View {
 }
 
 #Preview {
-    CountDownTimerText(text: "Send Again", timeCount: 10, timeUnit: "s"){
-        
+    VStack(spacing: 20) {
+        CountDownTimerText(timeCount: 10, timeUnit: "s", titleWhenIdle: "Get Code", titleWhenFinished: "Send Again", mode: .manual) {
+            
+        }
+        CountDownTimerText(timeCount: 10, timeUnit: "s", titleWhenIdle: "Get Code", titleWhenFinished: "Send Again", mode: .auto) {
+            
+        }
     }
 }
